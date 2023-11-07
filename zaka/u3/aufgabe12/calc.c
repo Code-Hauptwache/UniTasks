71a6de9cd8602f83bc54d560a20c474c3276ea97
zaka <zb2003boujana@gmail.com>
2023-11-07 22:44:07 +0100

    correction

--- zaka/u3/aufgabe12/calc.c
+++ zaka/u3/aufgabe12/calc.c
@@ -8,8 +8,8 @@ int main()
     printf("a) %f\n", 5 * a + 76 + 3.1);
     printf("b) %f\n", a + 6 / 2 * p * a + 1/a);
     printf("c) %f\n", a + b - p/c / a + c/a -1/b);
-    printf("d) %f\n", pow(a, -2));
-    printf("e) %f\n", pow((-a), 2));
+    printf("d) %f\n", 1 / (a * a));
+    printf("e) %f\n", -a * -a);
 
     return 0;
 }
