a56a88f0f80f8dedb878c9e25f4b7c35f42e71e0
zaka <zb2003boujana@gmail.com>
2023-11-07 23:44:20 +0100

    updated aufgabe13/calc.c

--- zaka/u3/aufgabe13/calc.c
+++ zaka/u3/aufgabe13/calc.c
@@ -5,9 +5,9 @@ int main()
     float a, b;
 
     printf("    The mini Calculator\n");
-    printf("Enter your first intiger: ");
+    printf("Enter your first number: ");
     scanf("%f", &a);
-    printf("Enter your second intiger: ");
+    printf("Enter your second number: ");
     scanf("%f", &b);
 
     printf("%f + %f = %f\n", a, b, a + b);
