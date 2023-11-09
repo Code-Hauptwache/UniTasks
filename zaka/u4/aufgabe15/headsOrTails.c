8983111bc3d99c398d9779e651ec4a7f282f5993
zaka <zb2003boujana@gmail.com>
2023-11-09 18:01:12 +0100

    zaka u4 aufgabe15

--- zaka/u4/aufgabe15/headsOrTails.c
+++ zaka/u4/aufgabe15/headsOrTails.c
@@ -17,7 +17,6 @@ int main()
 }
 
 //c)
-//  ohen Initialisierung des Zufallsgenerators wird nur 1 ausgegeben
-//  mit Initialisierung des Zufallsgenerators mit einem festen Wert
-// (z.B. 42, also srand(42)) wir nur 0 ausgegeben
-// mit Initialisierung des Zufallsgenerators mittels der aktuellen Systemzeit wird zufällig 0 oder 1 ausgegebe
+//  Ohen Initialisierung des Zufallsgenerators wird nur 1 ausgegeben
+//  Mit Initialisierung des Zufallsgenerators mit einem festen Wert (z.B. 42, also srand(42)) wir nur 0 ausgegeben
+//  Mit Initialisierung des Zufallsgenerators mittels der aktuellen Systemzeit wird zufällig 0 oder 1 ausgegebe
