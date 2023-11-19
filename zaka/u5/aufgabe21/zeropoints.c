5b58b4221269fb22ba581d0861c9a6a36bb4fde3
zaka <zb2003boujana@gmail.com>
2023-11-20 00:45:53 +0100

    u5

--- zaka/u5/aufgabe21/zeropoints.c
+++ zaka/u5/aufgabe21/zeropoints.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 
 int main()
-{}
+{
+    int a;
+    int b;
+    int c;
+
+    printf("\033[1mZeropoint calculator of a quadratic function ax^2+bx+c = 0\033[0m\n");
+    printf("Pleas enter a number for a: ");
+    scanf("%d", &a);
+    printf("Pleas enter a number for b: ");
+    scanf("%d", &b);
+    printf("Pleas enter a number for c: ");
+    scanf("%d", &c);
+
+
+}
