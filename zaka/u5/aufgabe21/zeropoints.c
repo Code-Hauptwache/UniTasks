fe2a40865106c8dac509431ae5b3b5bc13f091d9
zaka <zb2003boujana@gmail.com>
2023-11-20 01:43:09 +0100

    u5

--- zaka/u5/aufgabe21/zeropoints.c
+++ zaka/u5/aufgabe21/zeropoints.c
@@ -13,6 +13,23 @@ int main()
     scanf("%d", &b);
     printf("Pleas enter a number for c: ");
     scanf("%d", &c);
+    
+    if(a == 0 && b == 0 && c != 0)
+    {
+        printf("%ix^2+%ix+%i = 0 has no solution\n", a, b, c);
+        return 0;    
+    }
+    else if (a )
+    {
 
+    }   
 
+    // x = (-b +- sqrt(b^2 - 4ac)) / (2a))
+
+    double x1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
+    double x2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
+
+    printf("The zeropoints are: x1 = %f, x2 = %f\n", x1, x2);
+
+    return 0;
 }
