53b6e949b06d65638bd4924829b07a09495cbb3e
zaka <zb2003boujana@gmail.com>
2023-11-25 20:53:31 +0100

    u6

--- zaka/u6/aufgabe29/randomText.c
+++ zaka/u6/aufgabe29/randomText.c
@@ -1,6 +1,26 @@
+// LA ILALHA ILA ALLAAAAAAH
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
 int main()
 {
-    
+    int randSymbol;
+
+    srand(time(NULL));
+
+    while (1)
+    {
+        randSymbol = rand() % 94 + 33;
+
+        printf("%c", randSymbol);
+
+        if (randSymbol == 126)
+        {
+            fflush(stdout);
+        }
+
+        Sleep(rand() % 90 + 11);
+    }
 }
