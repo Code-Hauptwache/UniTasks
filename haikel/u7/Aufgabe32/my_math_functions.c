9262fc7ac7a70b48a9e5a5318ba1c707358eb31c
Achraf.akki <150199068+akkiwaiwa@users.noreply.github.com>
2023-11-29 11:10:08 +0100

    compiling, linking

---
+++ haikel/u7/Aufgabe32/my_math_functions.c
@@ -0,0 +1,16 @@
+#include "my_math_functions.h"
+int absWertInt(int v)
+{
+    if(v<0)
+        return -v;
+    else
+        return v;
+}
+
+ double absWertDouble (double v)
+{
+    if(v<0)
+        return -v;
+     else
+         return v;
+}
---
+++ haikel/u7/Aufgabe32/my_program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "my_math_functions.h"
+
+void main(void)
+{
+    int a=-3;
+    printf("Der Absolutwert von %d ist : %d\n", a, absWertInt(a));
+
+    double b = -2.7;
+    printf("Der Absolutwert von %lf ist : %lf\n",b , absWertDouble(b));
+}
