525bf9516b7403a38a33379a8cbd3d8f8a936664
zaka <zb2003boujana@gmail.com>
2023-12-08 22:02:56 +0100

    u8

--- zaka/u8/aufgabe32/factorial.c
+++ zaka/u8/aufgabe32/factorial.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <gmp.h>
 
 int main()
 {
     for (int i = 0; i < 51; i++)
     {
-        long double factorial = 1;
+        mpz_t trueFactorial;
+        mpz_init(trueFactorial);
+        mpz_fac_ui(trueFactorial, i);
+
+        unsigned long long factorial = 1;
 
         for (int j = 1; j <= i; j++)
         {
             factorial = factorial * j;
         }
 
-        printf("%d! = %.0Lf\n", i, factorial);
+        printf("%d! = %llu\n", i, factorial);
+        printf("The Corret answer to %d! is: ", i);
+        mpz_out_str(stdout, 10, trueFactorial);
+        printf("\n\n");
+
+        mpz_clear(trueFactorial);
     }
 }
+
+// b)
+//  Bis 20! ist das Ergebniss mit dem gößtem datentyp für positive Ganzzahlen (unsigned long long) korrekt.
