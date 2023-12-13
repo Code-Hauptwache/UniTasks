9b07499e5522871a6b1f99f85e10f99ead931c7c
Achraf.akki <150199068+akkiwaiwa@users.noreply.github.com>
2023-12-13 10:36:15 +0100

    u8

--- haikel/u8/Aufgabe34/aufgabe34.c
+++ haikel/u8/Aufgabe34/aufgabe34.c
@@ -3,5 +3,94 @@
 int main()
 //                                                           && || += -= *= <= >= usw.
 {
+    int beate;
+    int alfonso;
+    int christian;
+    int doris;
+    int alle;
 
+    printf("Choose your number : ");
+    scanf("%i", &beate);
+
+    if (beate >= 1)
+    {
+        printf("Wenn Beate geht, dann geht auch Christian mit.\n");
+    }
+     else if(beate < 1)
+    {
+        printf("Beate bleibt zuhause, wenn Alfonso und Christian gehen\n");
+    }
+    else
+    {
+        printf("Beate geht, dann geht Doris oder Christian.\n");
+    }
+
+    printf("Choose your second number : ");
+    scanf("%i", &alfonso);
+
+    if(alfonso < 0)
+    {
+        printf("Alfonso bleibt zuhause und es gehen entweder Doris oder Christian.\n");
+    }
+    else if(alfonso >=1)
+    {
+        printf("Wenn Alfonso zuhause bleibt, dann geht Doris,.\n");
+    }
+    else
+    {
+        printf("Wenn Alfonso zuhause bleibt und Doris auch, geht Christian.\n");
+    }
+
+    printf("Choose youre third number : ");
+    scanf("%i", &doris);
+
+    if(doris == 0)
+    {
+        printf("Doris geht mit Beate\n");
+    }
+    else if(doris >= 1)
+    {
+        printf("Wenn Doris geht, geht Alfonso auf keinen Fall.\n");
+    }
+    else
+    {
+        printf("Wenn Doris geht, geht Christian und Alfonso nicht, aber Beate.\n");
+    }
+
+    printf("Choose youre fourth number : ");
+    scanf("%i", &christian);
+
+    if(christian == 0)
+    {
+        printf("Wenn Alfonso und Christian gehen, geht Beate nicht hin.\n");
+    }
+    else if(christian > 0)
+    {
+        printf("Wenn Christian geht, dann geht auch Alfonso, aber Beate nicht.\n");
+    }
+    else
+    {
+        printf("Christian geht mit Doris, aber Alfonso kommt nicht mit.\n");
+    }
+
+    printf("Choose your fifth number : ");
+    scanf("%i", &alle);
+
+    if(alle == 0)
+    {
+        printf("Es geht mindestens nur Alfonso.\n");
+    }
+    else if(alle ==1)
+    {
+        printf("Es geht mindestens nur Beate.\n");
+    }
+    else if(alle == 2)
+    {
+        printf("Es geht mindestens nur Christian.\n");
+    }
+    else
+    {
+        printf("Es geht mindestens nur Doris.\n");
+    }
+        return 0;
 }
---
+++ haikel/u8/Aufgabe35/aufgabe35.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int Array[10];
+
+void ausgabe()
+{
+    int i;
+    for(i=0;i<=Array[10];i++)
+        printf("Ihre Elemente sind :  %i\n", Array[10]);
+
+}
+int main ()
+{
+    int sum;
+    int a;
+    printf("Die erste Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die zweite Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die dritte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die vierte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die fünfte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die sechste Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die siebte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die achte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die neunte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+        printf("Die zehnte Zahl deines Feldes : ");
+    scanf("%i", &Array[10]);
+    ausgabe();
+
+}
+
+
+
+
+
+
