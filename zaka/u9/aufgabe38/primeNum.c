e3c1defdb9618e8e4a227d43ed2d4a9b17ddd586
zakaBouj <zb2003boujana@gmail.com>
2024-01-31 22:08:54 +0100

    u9 a38

--- zaka/u9/aufgabe38/primeNum.c
+++ zaka/u9/aufgabe38/primeNum.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
 
-nextSmallerPrime(int n);
-nextLargerPrime(int n);
+int checkPrime(int num);
+int nextSmallerPrime(int num);
+int nextLargerPrime(int num);
 
 int main()
 {
     int num;
-    int checkPrime = 1;
 
     do
     {
-        printf("Please enter number lager than one: ");
-        scanf("%i", &num);
+        printf("Please enter a number larger than one: ");
+        if (scanf("%i", &num) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            while(getchar() != '\n'); // Clear the input buffer
+        }
     } while (num < 2);
 
-    for (int i = 2; i * i <= num; i++)
-    {
-        if (num % i == 0) checkPrime = 0;
-    }
-
-    if (checkPrime == 1)
+    if (checkPrime(num) && num > 2)
         printf("%i is a prime number. The next smaller prime of %i is %i and the next larger is %i\n", num, num, nextSmallerPrime(num), nextLargerPrime(num));
+    else if (checkPrime(num))
+        printf("%i is the smallest prime number. The next larger prime of %i is %i.\n", num, num, nextLargerPrime(num));
     else
         printf("%i is not a prime number\n", num);
+
+    return 0;
 }
 
-nextSmallerPrime(int n)
+int checkPrime(int num)
 {
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0) return 0;
+    }
 
+    return 1;
 }
 
-nextLargerPrime(int n)
+int nextSmallerPrime(int num)
 {
+    for (int i = 1; num - i > 1; i++)
+    {
+        if (checkPrime(num - i)) return num - i;
+    }
+    return -1; // Return -1 if no smaller prime is found (should not happen for num > 2)
+}
 
+int nextLargerPrime(int num)
+{
+    for (int i = 1; ; i++)
+    {
+        if (checkPrime(num + i)) return num + i;
+    }
 }
